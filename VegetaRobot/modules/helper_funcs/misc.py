735b76ccfcda85b72fd344352f11cf975da44192
DARKAMAN6 <89340128+DARKAMAN6@users.noreply.github.com>
2021-12-08 14:27:28 +0530

    Update misc.py

--- VegetaRobot/modules/helper_funcs/misc.py
+++ VegetaRobot/modules/helper_funcs/misc.py
@@ -66,9 +66,9 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     # can only have a certain amount of buttons side by side
     if len(pairs) > 100:
         pairs = pairs[modulo_page * 100:100 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("⬅️", callback_data="{}_prev({})".format(prefix, modulo_page)),
+            (EqInlineKeyboardButton("🌸", callback_data="{}_prev({})".format(prefix, modulo_page)),
                 EqInlineKeyboardButton("Back to Home", callback_data="vegeta_back"),
-             EqInlineKeyboardButton("➡️", callback_data="{}_next({})".format(prefix, modulo_page)))]
+             EqInlineKeyboardButton("🌸", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
         pairs += [[EqInlineKeyboardButton("⬅ Back to Home ➡", callback_data="vegeta_back")]]
